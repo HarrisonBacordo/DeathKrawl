7c5c3d501f59da28548f3b2c8ca3aad77ddbdab2
dknz2008 <dylan.kumar@icloud.com>
2017-09-27 13:55:22 +1300

    added tests

---
+++ src/main/java/AI/GrappleAI.java
@@ -0,0 +1,84 @@
+package AI;
+
+import Entity.Entity;
+import Entity.EntityType;
+import Component.Component;
+import LevelGenerator.Rooms.Room;
+
+import java.awt.*;
+/**
+ * Created by kumardyla on 18/09/17.
+ */
+public class GrappleAI extends Entity {
+
+    State currentState;
+    States state;
+    FacingDirection facingDirection;
+    EntityDetectorComponent detection;
+    Room currentRoom;
+    Entity opponent;
+
+    public GrappleAI(int x, int y, int width, int height, States state, long ID, FacingDirection fd, Entity player, Room currentRoom) {
+        super(x, y, width, height, EntityType.ENEMY, ID);
+
+        this.currentState = new GrappleState(this, currentRoom, player);
+
+        this.state = state;
+
+        //components.addComponent(new EntityDetectorComponent(this, player));
+        detection = new EntityDetectorComponent(this, player, 100);
+        this.facingDirection = fd;
+        this.opponent = player;
+        this.currentRoom = currentRoom;
+        System.out.println("CONSTRUCTED");
+    }
+
+    public void setState(State state){
+        this.currentState = state;
+    }
+
+    @Override
+    public void tick(){
+
+        if(currentState != null){
+
+            currentState.execute();
+//            System.out.println(( (GrappleState)(currentState)).getEndX());
+
+            if(state == States.MOVETOWARDS){
+                //if within a certain distance, change state to attack
+                //if lost player, change state to searching
+                if(!detection.CheckIfInView()){
+                    state = States.GRAPPLE;
+                    currentState = new GrappleState(this, currentRoom, opponent);
+                }
+            }else if(state == States.ATTACK){
+                //explode
+            }else if(state == States.GRAPPLE){
+                //System.out.println(( (GrappleState)(currentState)).getEndX());
+//                if(detection.CheckIfInView()){
+//                    state = States.MOVETOWARDS;
+//                    currentState = new MoveTowardsState(this, currentRoom, opponent);
+//                }
+            }
+        }
+
+
+    }
+
+    @Override
+    public void render(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.GREEN);
+
+        currentState.draw(g2d, x, y, width, height);
+        detection.draw(g2d);
+        //g2d.fillRect(x, y, width, height);
+    }
+
+    @Override
+    public Rectangle getBoundingBox(){
+        return new Rectangle(x, y, width, height);
+    }
+
+}
