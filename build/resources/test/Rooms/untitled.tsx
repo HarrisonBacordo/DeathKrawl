5732085e0b4564200f60e43757aad26e8818bbd8
Harrison Bacordo <harrisonbacordo@gmail.com>
2017-09-27 23:21:18 +1300

    removes build folder from working directory

--- build/resources/test/Rooms/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="32x32" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../../../../Desktop/32x32.png" width="512" height="512"/>
-</tileset>
