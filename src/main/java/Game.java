40e45e937bb1f11ff447fe31870be473a53a5cc9
Krishna Kapadia <kapadikris@myvuw.ac.nz>
2017-09-28 19:27:23 +1300

    Added scaling to fullscreen on all resolutions

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -15,8 +15,8 @@ import java.util.Random;
 
 public class Game extends Canvas implements Runnable{
     public static final int WINDOW_WIDTH = 960;
-    public static final int WINDOW_HEIGHT = 565;
-
+    public static final int WINDOW_HEIGHT = 544;
+    private double scaleFactorX, scaleFactorY;
     private boolean isRunning;
     private Thread thread;
     private KeyInput inputHandler;
@@ -30,7 +30,10 @@ public class Game extends Canvas implements Runnable{
     private HeadsUpDisplay HUD;
 
     public Game(){
-        Window w = new Window(WINDOW_WIDTH, WINDOW_HEIGHT, "This is my game", this); // 960 x 540
+        Window w = new Window("This is my game", this); // 960 x 540
+        //Calculate Scale factor
+        scaleFactorX = (w.getWidth() / WINDOW_WIDTH);
+        scaleFactorY = (w.getHeight() / WINDOW_HEIGHT);
 
         resourceManager = new Resources();
         inputHandler = new KeyInput();
@@ -42,7 +45,7 @@ public class Game extends Canvas implements Runnable{
         stateM = new StateManager(inputHandler);
         //LEVEL INIT
         level = new Level(15, 960, 544);
-        camera = new Camera(level.getCurrentRoom().getX(), level.getCurrentRoom().getY(), 960 * 2, 565 * 2);
+        camera = new Camera(level.getCurrentRoom().getX(), level.getCurrentRoom().getY(), 960, 544);
 
 //        -----------------HUD-----------------
         HUD = new HeadsUpDisplay(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -137,12 +140,11 @@ public class Game extends Canvas implements Runnable{
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.scale(scaleFactorX, scaleFactorY);
         ///////////RENDER IN HERE////////////
 
         //Temp background
-
-
-
 
         if(stateM.getState() == STATE.GAME) {//Temp background
             g.setColor(new Color(66, 40, 53));
