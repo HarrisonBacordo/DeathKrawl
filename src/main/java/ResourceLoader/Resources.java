7aca8a841b2f14777cb8eea3eb154718f187dd26
Harrison Bacordo <bacordharr@myvuw.ac.nz>
2017-10-02 12:12:45 +1300

    hotfix for muzzle flash. delets unnecessary pngs. Better organizes resource class

--- src/main/java/ResourceLoader/Resources.java
+++ src/main/java/ResourceLoader/Resources.java
@@ -33,6 +33,7 @@ public class Resources {
         infoButton = new BufferedImage[12];
         loadButton = new BufferedImage[12];
         saveButton = new BufferedImage[12];
+//        -------------------- MENU ANIMATION PNG --------------------
         for(int i = 0; i<12;i++){
             playButton[i] = Loader.loadImage("/Menu/playAni/playAni"+(i+1)+".png");
         }
@@ -50,25 +51,32 @@ public class Resources {
         }
         assets.put("mainT", Loader.loadImage("/Menu/titles/mainTitle.png"));
         assets.put("pauseT", Loader.loadImage("/Menu/titles/pauseTitle.png"));
-        //load the image, break down with sprite, extract here
+
+//        -------------------- DOOR, BOSSROOM, PLAYER PNG --------------------
         assets.put("SpawnRoom", Loader.loadImage("/Rooms/SpawnRoomWDoor.png"));
         assets.put("BossRoom", Loader.loadImage("/Rooms/BossRoomSmall.png"));
         assets.put("Player", Loader.loadImage("/player.png"));
-        //Adds all variations
+
+//        -------------------- ROOM VARIATION PNG --------------------
         for(int i = 0; i < variations; i++) {
             assets.put("Room" + (i + 1), Loader.loadImage("/Rooms/RoomVar" + (i + 1) + ".png"));
         }
+
+//        -------------------- FLOOR PNG --------------------
         assets.put("FT", Loader.loadImage("/Enviroment/floorTile.png"));
 
+//        -------------------- WALL PNG --------------------
         assets.put("WT", Loader.loadImage("/Enviroment/wallTile.png"));
         assets.put("WTL", Loader.loadImage("/Enviroment/wallTileLeft.png"));
         assets.put("WTR", Loader.loadImage("/Enviroment/wallTileRight.png"));
         assets.put("WTB", Loader.loadImage("/Enviroment/wallTileBottom.png"));
 
+//        -------------------- FLOOR SHADOW PNG --------------------
         assets.put("FSL", Loader.loadImage("/Enviroment/floorShadowLeft.png"));
         assets.put("FSR", Loader.loadImage("/Enviroment/floorShadowRight.png"));
         assets.put("FS", Loader.loadImage("/Enviroment/floorShadow.png"));
 
+//        -------------------- SEA TILE PNG --------------------
         assets.put("SEA", Loader.loadImage("/Enviroment/seaTile.png"));
         assets.put("SEAT", Loader.loadImage("/Enviroment/seaTileTop.png"));
         assets.put("SEAL", Loader.loadImage("/Enviroment/seaTileLeft.png"));
@@ -79,19 +87,23 @@ public class Resources {
         assets.put("SEATL", Loader.loadImage("/Enviroment/seaTileTopLeftCorner.png"));
         assets.put("SEATR", Loader.loadImage("/Enviroment/seaTileTopRightCorner.png"));
 
+//        -------------------- ITEMS PNG --------------------
         assets.put("PISTOL", Loader.loadImage("/HUD/defaultGun.png"));
         assets.put("SHOTGUN", Loader.loadImage("/HUD/shotgun.png"));
         assets.put("HEART", Loader.loadImage("/HUD/heart.png"));
         assets.put("SWORD", Loader.loadImage("/HUD/sword.png"));
-
         assets.put("SHIELD", Loader.loadImage("/Items/SHEILD.png"));
         assets.put("ASSAULT_RIFLE", Loader.loadImage("/Items/ASSAULT_RIFLE.png"));
         assets.put("SPEEDBOOST", Loader.loadImage("/Items/SPEEDBOOST 2.png"));
 
+//        -------------------- SWORD SLASH PNG --------------------
         assets.put("SLASHT", Loader.loadImage("/Sword/slashTop.png"));
         assets.put("SLASHB", Loader.loadImage("/Sword/slashBottom.png"));
         assets.put("SLASHL", Loader.loadImage("/Sword/slashLeft.png"));
         assets.put("SLASHR", Loader.loadImage("/Sword/slashRight.png"));
+
+//        -------------------- MUZZLE FLASH PNG --------------------
+        assets.put("MUZZLE", Loader.loadImage("/MuzzleFlash/muzzleFlash.png"));
     }
 
     public static HashMap<String, BufferedImage> getAssets() {
